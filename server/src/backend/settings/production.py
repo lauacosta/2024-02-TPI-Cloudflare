f113a5a8bea8d0feaf4ecabe1760e4e6925130f7
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2024-11-22 17:56:56 -0300

    Arregle un error tonto

--- server/src/backend/settings/production.py
+++ server/src/backend/settings/production.py
@@ -5,7 +5,9 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = [host.strip() for host in config("WEBSITE_HOSTNAME", default="localhost").split(",")]
+ALLOWED_HOSTS = [
+    host.strip() for host in config("WEBSITE_HOSTNAME", default="localhost").split(",")
+]
 
 
 DATABASES = {"default": config("DATABASE_URL", cast=dj_database_url.parse)}
