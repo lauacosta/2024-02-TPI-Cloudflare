76a23e13bad6fb7ae784c9e2dd9e33169a536154
Lautaro Acosta Quintana <acostaquintanalautaro@gmail.com>
2024-11-21 23:29:02 -0300

    .

--- server/src/backend/settings/production.py
+++ server/src/backend/settings/production.py
@@ -5,7 +5,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = [host.strip() for host in config("ALLOWED_HOSTS", default="localhost").split(",")]
+ALLOWED_HOSTS = [host.strip() for host in config("ALLOWED_HOSTS", default="localhost,https://tpi-desarrollo-e0f8gccuhvhpbkhj.eastus-01.azurewebsites.net/").split(",")]
 
 
 DATABASES = {"default": config("DATABASE_URL", cast=dj_database_url.parse)}
